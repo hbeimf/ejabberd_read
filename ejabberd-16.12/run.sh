ca0ef7125bf240b3d9cf6a977d18fb286ad7cfa3
maofeng <maofeng@jrq.com>
2017-05-23 17:07:21 +0800

    node & cookie & ctl

--- ejabberd-16.12/run.sh
+++ ejabberd-16.12/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-EJABBERD_CONFIG_PATH=ejabberd.yml erl -pa ebin -pa deps/*/ebin -pa test -pa deps/elixir/lib/*/ebin/ -s ejabberd
+EJABBERD_CONFIG_PATH=ejabberd.yml erl -pa ebin -pa deps/*/ebin -pa test -pa deps/elixir/lib/*/ebin/ -s ejabberd \
+ -name ejabberd@127.0.0.1 -setcookie ejabberd_cookie
 
 
